3a7d134e5804e3435945fbcdc2c050d90b7a8ddf
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-24 02:09:38 +0530

    db connection success

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 const PORT = 4000;
+
+await mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('db connected'))
+    .catch((er) => console.log('db connection issue'));
 
 app.post('/register', (req, res) => {
     const { username, password } = req.body;
