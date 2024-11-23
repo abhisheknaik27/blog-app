9997a1773af9c4a517e21b2ca2d23b1648e0c347
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-24 03:00:57 +0530

    password hashed using bcryptjs

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import bcrypt from 'bcryptjs';
 import UserModel from './models/User.js';
-
 dotenv.config();
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -18,10 +21,16 @@ await mongoose.connect(process.env.MONGO_URL)
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-    const user = await UserModel.create({
-        username, password
-    })
-    res.json(user);
+    try{
+        const user = await UserModel.create({
+            username, 
+            password: bcrypt.hashSync(password, salt)
+        })
+        res.json(user);
+    }catch(err){
+        res.status(400).json({err});
+    }
+    
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -7,13 +7,17 @@ const Register = () => {
 
   const register = async (e) => {
     e.preventDefault();
-    await fetch("http://localhost:4000/register", {
+
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
-    }).then((res) => {
-      console.log(res.data);
     });
+    if (response.status == 200) {
+      alert("Registration Success");
+    } else {
+      alert("Registration Failed");
+    }
   };
   return (
     <form className="max-w-[400px] m-auto pt-20" onSubmit={register}>
